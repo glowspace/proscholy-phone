437e1d1fa7936a24ff738e7c4c907f0be9ff2ff0
Paprikadobi <patrikdobidobias@gmail.com>
2022-07-20 21:40:30 +0200

    fix statusbar in dark mode

--- lib/screens/content.dart
+++ lib/screens/content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:zpevnik/constants.dart';
 import 'package:zpevnik/providers/navigation.dart';
@@ -10,55 +11,60 @@ class ContentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     final navigationProvider = context.read<NavigationProvider>();
     final isFullScreen = context.select<NavigationProvider, bool>((provider) => provider.isFullScreen);
 
-    return WillPopScope(
-      onWillPop: () async => !(await navigationProvider.navigatorKey.currentState?.maybePop() ?? false),
-      child: LinksHandlerWrapper(
-        child: LayoutBuilder(
-          builder: (_, constraints) {
-            if (constraints.maxWidth > kTabletSizeBreakpoint && constraints.maxHeight > kTabletSizeBreakpoint) {
-              return Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AnimatedAlign(
-                    alignment: Alignment.center,
-                    duration: kDefaultAnimationDuration,
-                    widthFactor: isFullScreen ? 0 : 1,
-                    child: ClipRect(
-                      clipBehavior: Clip.antiAlias,
-                      child: SizedBox(
-                        width: 320,
-                        child: Navigator(
-                          key: navigationProvider.menuNavigatorKey,
-                          observers: [navigationProvider.menuNavigatorObserver!, HeroController()],
-                          onGenerateRoute: MenuRouteGenerator.generateRoute,
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(statusBarBrightness: theme.brightness),
+      child: WillPopScope(
+        onWillPop: () async => !(await navigationProvider.navigatorKey.currentState?.maybePop() ?? false),
+        child: LinksHandlerWrapper(
+          child: LayoutBuilder(
+            builder: (_, constraints) {
+              if (constraints.maxWidth > kTabletSizeBreakpoint && constraints.maxHeight > kTabletSizeBreakpoint) {
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AnimatedAlign(
+                      alignment: Alignment.center,
+                      duration: kDefaultAnimationDuration,
+                      widthFactor: isFullScreen ? 0 : 1,
+                      child: ClipRect(
+                        clipBehavior: Clip.antiAlias,
+                        child: SizedBox(
+                          width: 320,
+                          child: Navigator(
+                            key: navigationProvider.menuNavigatorKey,
+                            observers: [navigationProvider.menuNavigatorObserver!, HeroController()],
+                            onGenerateRoute: MenuRouteGenerator.generateRoute,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const VerticalDivider(width: 0),
-                  Expanded(
-                    child: ClipRect(
-                      clipBehavior: Clip.antiAlias,
-                      child: Navigator(
-                        key: navigationProvider.navigatorKey,
-                        observers: [navigationProvider.navigatorObserver, HeroController()],
-                        onGenerateRoute: RouteGenerator.generateRoute,
+                    const VerticalDivider(width: 0),
+                    Expanded(
+                      child: ClipRect(
+                        clipBehavior: Clip.antiAlias,
+                        child: Navigator(
+                          key: navigationProvider.navigatorKey,
+                          observers: [navigationProvider.navigatorObserver, HeroController()],
+                          onGenerateRoute: RouteGenerator.generateRoute,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            }
+                  ],
+                );
+              }
 
-            return Navigator(
-              key: navigationProvider.navigatorKey,
-              observers: [navigationProvider.navigatorObserver, HeroController()],
-              onGenerateRoute: RouteGenerator.generateRoute,
-            );
-          },
+              return Navigator(
+                key: navigationProvider.navigatorKey,
+                observers: [navigationProvider.navigatorObserver, HeroController()],
+                onGenerateRoute: RouteGenerator.generateRoute,
+              );
+            },
+          ),
         ),
       ),
     );
