98b1d0403199ae227b256cf73cb738a6c3376d4e
Paprikadobi <patrikdobidobias@gmail.com>
2022-08-06 08:18:07 +0200

    now playing section handling no internet

--- lib/components/home/now_playing_section.dart
+++ lib/components/home/now_playing_section.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:zpevnik/components/highlightable.dart';
 import 'package:zpevnik/constants.dart';
+import 'package:zpevnik/models/song_lyric.dart';
 import 'package:zpevnik/providers/data.dart';
 import 'package:zpevnik/routes/arguments/song_lyric.dart';
 import 'package:zpevnik/utils/extensions.dart';
@@ -20,18 +25,27 @@ class NowPlayingSection extends StatefulWidget {
 }
 
 class _NowPlayingSectionState extends State<NowPlayingSection> {
-  late final WebSocketChannel _channel;
+  late WebSocketChannel _channel;
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  int reconnectTries = 0;
+
+  SongLyric? _songLyric;
 
   @override
   void initState() {
     super.initState();
 
-    _channel = WebSocketChannel.connect(_wssUri);
-    _channel.sink.add('ask');
+    _connectSocket(context.read<DataProvider>());
+
+    _connectivitySubscription = Connectivity().onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.none) _channel.sink.close();
+    });
   }
 
   @override
   void dispose() {
+    _connectivitySubscription.cancel();
     _channel.sink.close();
 
     super.dispose();
@@ -41,54 +55,75 @@ class _NowPlayingSectionState extends State<NowPlayingSection> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return StreamBuilder(
-      stream: _channel.stream,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return Container();
-
-        final songLyric = context.read<DataProvider>().getSongLyricById(int.tryParse(snapshot.data as String) ?? -1);
-
-        if (songLyric == null) return Container();
-
-        return Container(
-          margin: const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-          clipBehavior: Clip.antiAlias,
-          child: Highlightable(
-            onTap: () => Navigator.of(context).pushNamed(
-              '/song_lyric',
-              arguments: SongLyricScreenArguments([songLyric], 0),
-            ),
-            padding: const EdgeInsets.fromLTRB(2 * kDefaultPadding, kDefaultPadding, kDefaultPadding, kDefaultPadding),
-            color: theme.brightness.isLight ? _backgroundColor : _backgroundHighlightColor,
-            highlightColor: theme.brightness.isLight ? _backgroundHighlightColor : _backgroundColor,
-            highlightBackground: true,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Právě teď na CSM',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color: Colors.white),
-                      ),
-                      const SizedBox(height: kDefaultPadding / 2),
-                      Text(
-                        songLyric.name,
-                        style: const TextStyle(fontSize: 19, fontWeight: FontWeight.w500, color: Colors.white),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
+    return AnimatedCrossFade(
+      duration: kDefaultAnimationDuration,
+      crossFadeState: _songLyric == null ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+      firstChild: Container(),
+      secondChild: Container(
+        margin: const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+        clipBehavior: Clip.antiAlias,
+        child: Highlightable(
+          onTap: _songLyric == null
+              ? null
+              : () => Navigator.of(context).pushNamed(
+                    '/song_lyric',
+                    arguments: SongLyricScreenArguments([_songLyric!], 0),
                   ),
+          padding: const EdgeInsets.fromLTRB(2 * kDefaultPadding, kDefaultPadding, kDefaultPadding, kDefaultPadding),
+          color: theme.brightness.isLight ? _backgroundColor : _backgroundHighlightColor,
+          highlightColor: theme.brightness.isLight ? _backgroundHighlightColor : _backgroundColor,
+          highlightBackground: true,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Právě teď na CSM',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color: Colors.white),
+                    ),
+                    const SizedBox(height: kDefaultPadding / 2),
+                    Text(
+                      _songLyric?.name ?? '',
+                      style: const TextStyle(fontSize: 19, fontWeight: FontWeight.w500, color: Colors.white),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
                 ),
-                const SizedBox(width: kDefaultPadding),
-                Image.asset('assets/images/logos/csmhk.png', height: 56),
-              ],
-            ),
+              ),
+              const SizedBox(width: kDefaultPadding),
+              Image.asset('assets/images/logos/csmhk.png', height: 56),
+            ],
           ),
-        );
+        ),
+      ),
+    );
+  }
+
+  void _connectSocket(DataProvider dataProvider) {
+    print('connecting socket');
+
+    _channel = WebSocketChannel.connect(_wssUri);
+    _channel.sink.add('ask');
+
+    _channel.stream.listen(
+      (event) {
+        reconnectTries = 0;
+        setState(() => _songLyric = dataProvider.getSongLyricById(int.tryParse(event as String) ?? -1));
+      },
+      onError: (_) => {},
+      onDone: () {
+        print('waiting for ${Duration(seconds: min(60, pow(2, reconnectTries).toInt()))}');
+
+        if (reconnectTries > 5) setState(() => _songLyric = null);
+
+        // _channel.sink.close();
+
+        Future.delayed(
+            Duration(seconds: min(60, pow(2, reconnectTries++).toInt())), () => _connectSocket(dataProvider));
       },
     );
   }
