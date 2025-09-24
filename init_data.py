99a6f81b1c76322164ed158cdfc952da4c684382
Paprikadobi <patrikdobidobias@gmail.com>
2025-09-24 16:43:08 +0200

    add arrangement to song_lyric

--- init_data.py
+++ init_data.py
@@ -44,6 +44,7 @@ song_lyrics_data = {
                 lang
                 lang_string
                 type_enum
+                is_arrangement
                 has_chords
                 song {
                     id
